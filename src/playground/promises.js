5411d1bfdd7defaefc18cc4ba636edbd24ec2f42
Rahul Pisal <rpisal@gmail.com>
2017-11-28 16:11:56 +0400

    Code completed till lecture 149

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,13 +1,19 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('This is my resolved data.');
+        // resolve({
+        //     name: 'Rahul',
+        //     age: 41
+        // });
+        reject('Something went wrong');
     }, 5000);
 });
 
 console.log('before');
 
 promise.then((data) => {
-    console.log(data);
+    console.log('1', data);
+}).catch((error) => {
+    console.log(error);
 });
 
 console.log('after');
