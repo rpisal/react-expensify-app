25034b22cfd04eb754e31cb8e7b73c7f2765af07
Rahul Pisal <rpisal@gmail.com>
2017-11-29 15:34:27 +0400

    code completed upto lecture 162

--- src/app.js
+++ src/app.js
@@ -9,17 +9,9 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
@@ -33,4 +25,10 @@ store.dispatch(startSetExpenses()).then(() => {
   ReactDOM.render(jsx, document.getElementById('app'));
 });
 
-
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    console.log('log in');
+  } else {
+    console.log('log out');
+  }
+ });
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import * as firebase from 'firebase';
-import { setTimeout } from 'timers';
 
 // Initialize Firebase
 const config = {
@@ -15,4 +14,6 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-export { firebase, database as default };
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
